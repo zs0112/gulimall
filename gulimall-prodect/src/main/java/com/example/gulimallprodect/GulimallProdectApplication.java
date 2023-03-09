b9f46e3c9710cd6ba9383646e03509ef1a87cd53
zs0112 <1958929104@qq.com>
2023-03-09 09:42:23 +0800

    1.0

---
+++ gulimall-prodect/src/main/java/com/example/gulimallprodect/GulimallProdectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gulimallprodect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallProdectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProdectApplication.class, args);
+    }
+
+}
---
+++ gulimall-prodect/src/test/java/com/example/gulimallprodect/GulimallProdectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gulimallprodect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProdectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
